14f643d4cde76e437f27e40ae7fd336fb5af1495
Klevis Imeri <klevisimeri11@gmail.com>
2026-04-06 18:32:16 +0200

    added the op alias for opencode

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -2,3 +2,4 @@ alias la='ls -a'
 alias ll='ls -alh'
 alias pactl-hdmi='pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo && pactl set-sink-port @DEFAULT_SINK@ hdmi-output-0'
 alias xclip="xclip -selection c"
+alias op="opencode --port"
